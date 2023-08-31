0fbafba197fd7d389a9362804e75c5bfad9b696d
Rishav Nath Pati <patirishavnath@gmail.com>
2023-08-31 22:01:55 +0530

    Initial Commit + Scripts, Assets

---
+++ CommandPattern/Assets/Scripts/CameraFollow360.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class CameraFollow360 : MonoBehaviour
+{
+    public Transform player;
+    public float distance = 10;
+    public float height = 5;
+    public Vector3 lookOffset = new(0, 1, 0);
+    private const float CameraSpeed = 100;
+    private const float RotSpeed = 100;
+
+    private void FixedUpdate()
+    {
+        if (player)
+        {
+            var lookPosition = player.position + lookOffset;
+            var relativePos = lookPosition - transform.position;
+            var rot = Quaternion.LookRotation(relativePos);
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, rot, Time.deltaTime * RotSpeed * 0.1f);
+
+            var targetPos = player.transform.position + player.transform.up * height -
+                            player.transform.forward * distance;
+
+            transform.position = Vector3.Lerp(transform.position, targetPos, Time.deltaTime * CameraSpeed * 0.1f);
+        }
+    }
+}
---
+++ CommandPattern/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private const float RotationSpeed = 100.0f;
+    private const float Speed = 2.0f;
+    private static readonly int IsWalking = Animator.StringToHash("isWalking");
+    private static readonly int IsJumping = Animator.StringToHash("isJumping");
+    private static readonly int IsPunching = Animator.StringToHash("isPunching");
+    private static readonly int IsKicking = Animator.StringToHash("isKicking");
+    private Animator _anim;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    private void LateUpdate()
+    {
+        var translation = Input.GetAxis("Vertical") * Speed;
+        var rotation = Input.GetAxis("Horizontal") * RotationSpeed;
+
+        // Make it move 10 meters per second instead of 10 meters per frame...
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+
+        if (Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.DownArrow) ||
+            Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow))
+            _anim.SetBool(IsWalking, false);
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow))
+        {
+            _anim.SetBool(IsWalking, true);
+            transform.Translate(0, 0, translation);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+        {
+            _anim.SetBool(IsWalking, true);
+            transform.Rotate(0, rotation, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            _anim.SetTrigger(IsJumping);
+        else if (Input.GetKeyDown(KeyCode.P))
+            _anim.SetTrigger(IsPunching);
+        else if (Input.GetKeyDown(KeyCode.K))
+            _anim.SetTrigger(IsKicking);
+    }
+}
